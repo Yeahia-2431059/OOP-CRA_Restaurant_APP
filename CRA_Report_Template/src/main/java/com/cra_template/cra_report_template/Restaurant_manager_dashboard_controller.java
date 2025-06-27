ccda75b285a2f0fa22be58e0f34e68c5d1f079a9
Yeahia-2431059 <2431059@iub.edu.bd>
2025-06-27 11:03:11 +0600

    Delete CRA_Report_Template directory

--- CRA_Report_Template/src/main/java/com/cra_template/cra_report_template/Restaurant_manager_dashboard_controller.java
+++
@@ -1,6 +0,0 @@
-package com.cra_template.cra_report_template;
-
-import javafx.fxml.FXML;
-
-public class Restaurant_manager_dashboard_controller {
-}
--- CRA_Report_Template/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.cra_template.cra_report_template {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.cra_template.cra_report_template to javafx.fxml;
-    exports com.cra_template.cra_report_template;
-}
